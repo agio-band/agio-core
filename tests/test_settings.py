4289e74372a280a88f08c184a26ea15e007de2de
admin <paulwinex@gmail.com>
2025-05-03 21:54:17 +0300

    add first test

---
+++ tests/test_settings.py
@@ -0,0 +1,24 @@
+import pytest
+from agio.core.settings import APackageSettings, IntField
+from agio.core.settings.exceptions import RequiredValueNotSetError, ValueTypeError
+
+
+def test_int_field():
+    class TestSettings(APackageSettings):
+        value: IntField
+
+    with pytest.raises(RequiredValueNotSetError):
+        _ = TestSettings()
+    inst = TestSettings(value=10)
+    assert inst.value.get() == 10
+    inst.value.set(20)
+    assert inst.value.get() == 20
+
+    with pytest.raises(ValueTypeError):
+        inst.value.set('notvalid')
+
+    with pytest.raises(ValueTypeError):
+        _ = TestSettings(value='notvalid')
+    with pytest.raises(ValueTypeError):
+        _ = TestSettings(value=[1,2,3])
+
