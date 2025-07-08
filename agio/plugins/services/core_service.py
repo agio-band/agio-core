f147c9b027c70a4eca929a29e14b002538dabb04
admin <paulwinex@gmail.com>
2025-07-08 22:57:23 +0300

    fix core service name

--- agio/plugins/services/core_service.py
+++ agio/plugins/services/core_service.py
@@ -5,18 +5,18 @@ import os
 
 
 class CoreService(ServicePlugin):
-    name = 'core_actions'
+    name = 'core'
     def execute(self, **kwargs):
         pass
 
-    @action(menu_name='tray.main_menu', app_name='desk', order=100)
-    def exit(self):
-        os.kill(os.getpid(), signal.SIGINT)
+    @action(menu_name='tray.main_menu', app_name='desk', order=98)
+    def login(self):
+        client.logout()
 
     @action(menu_name='tray.main_menu', app_name='desk', order=99)
     def login(self):
         client.login()
 
-    @action(menu_name='tray.main_menu', app_name='desk', order=98)
-    def login(self):
-        client.logout()
+    @action(menu_name='tray.main_menu', app_name='desk', order=100)
+    def exit(self):
+        os.kill(os.getpid(), signal.SIGINT)
