5d1a32f531230338bbe085aff63c559756492bd7
admin <paulwinex@gmail.com>
2025-09-06 00:27:12 +0300

    added extra_context_args

--- agio/core/cli/setup_commands.py
+++ agio/core/cli/setup_commands.py
@@ -1,12 +1,37 @@
 import logging
 import os
 import sys
+from collections.abc import Callable
+from typing import Iterable
+
 import click
 
 from agio.core.utils  import plugin_hub
 from agio.core.pkg.workspace import AWorkspaceManager
 from agio.core.utils import launch_utils
 
+
+def extra_context_args():
+    """
+    Collect context args from plugins
+    """
+    plugins = list(plugin_hub.APluginHub.instance().get_plugins_by_type('extra_context_args'))
+    if plugins:
+        def inter_options() -> Iterable[Callable]:
+            for plg in plugins:
+                args = plg.get_args()
+                if not isinstance(args, Iterable):
+                    args = [args]
+                yield from args
+
+        def decorator(f):
+            for opt in inter_options():
+                f = opt(f)
+            return f
+    else:
+        def decorator(f):
+            return f
+    return decorator
 
 # base command
 @click.group(name='agio')
@@ -19,12 +44,13 @@ from agio.core.utils import launch_utils
     help='Execute in workspace (Workspace ID, Revision ID, Settings ID, Project ID)',
     required=False
 )
+@extra_context_args()
 @click.pass_context
-def agio_group(ctx, workspace, debug):
-    if not AWorkspaceManager.is_defined() and workspace:
-        # execute in different env if not current ws is defined
-        ws: AWorkspaceManager|None = launch_utils.get_launch_context_from_args(workspace)
+def agio_group(ctx, workspace, debug, **kwargs):
+    if not AWorkspaceManager.is_defined():
+        ws: AWorkspaceManager|None = create_launch_context(workspace, debug, kwargs)
         if ws:
+            # execute in different env if not current ws is defined
             command_args = launch_utils.clear_args(sys.argv)
             launch_utils.start_in_workspace(command_args, ws)
             ctx.exit()
@@ -32,9 +58,17 @@ def agio_group(ctx, workspace, debug):
         os.environ['DEBUG'] = 'true'
 
 
+def create_launch_context(workspace_id: str, debug: bool, extra_kwargs):
+    ws: AWorkspaceManager | None = launch_utils.get_launch_context_from_args(workspace_id)
+    for plg in plugin_hub.APluginHub.instance().get_plugins_by_type('extra_context_args'):
+        # any context plugin can add extra envs and other modifications
+        plg.execute(ws, **extra_kwargs)
+    return ws
+
+
 @agio_group.command
 def ping():
-    click.echo('pong')
+    click.secho('PONG', fg='green')
 
 
 def load_plugins():
