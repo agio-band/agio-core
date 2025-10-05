1ee973df9834086ab8b7b33215b596c1d635dcfe
admin <paulwinex@gmail.com>
2025-10-05 15:02:25 +0300

    add install script for linux

---
+++ scripts/agio-install.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+venv_dir=~/.agio/venv
+mkdir -p $venv_dir 
+
+pyexec=$(which python3 2>/dev/null)
+
+if [[ -z "$pyexec" ]]; then
+    echo "python3 not found in PATH" >&2
+    exit 1
+fi
+
+$pyexec -m venv $venv_dir
+$venv_dir/bin/python -m pip install -U pip
+$venv_dir/bin/python -m pip install /home/paul/pw-storage/dev/work/agio/packages/agio-core
+
+bin_file=~/.local/bin/agio
+if [[ -e "$bin_file" ]]; then
+    rm -rf "$bin_file"
+    if [[ $? -eq 0 ]]; then
+        echo .
+    else
+        echo "Update binary error"
+        exit
+    fi
+fi
+ln -s $venv_dir/bin/agio $bin_file
