df5e60b523468d924f55ce2c47c384fd72be99f6
admin <paulwinex@gmail.com>
2025-10-11 15:58:27 +0300

    use signal on startup for main thread only

--- agio/core/__init__.py
+++ agio/core/__init__.py
@@ -1,13 +1,14 @@
+import atexit
 import logging
 import signal
+import threading
 
-from .events import subscribe as _subscribe
 from .events import emit as _emit
+from .events import subscribe as _subscribe
 from .init.init_packages import init_packages
 from .init.init_plugins import init_plugins
-from .utils import setup_logger
 from .utils import process_hub
-
+from .utils import setup_logger
 
 logger = logging.getLogger(__name__)
 init_packages()
@@ -22,9 +23,11 @@ def _before_exit_event(*args):
     logger.debug('Receive exit signal')
     _emit('core.app.exit')
 
-
-signal.signal(signal.SIGINT, _before_exit_event)
-signal.signal(signal.SIGTERM, _before_exit_event)
+if threading.current_thread() is threading.main_thread():
+    signal.signal(signal.SIGINT, _before_exit_event)
+    signal.signal(signal.SIGTERM, _before_exit_event)
+else:
+    atexit.register(_before_exit_event)
 
 _emit('core.app.on_startup')
 logger.debug('Core init done')
