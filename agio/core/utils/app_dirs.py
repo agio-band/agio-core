5636041ea5bcb746716013b32bd8b052e4242481
admin <paulwinex@gmail.com>
2025-10-16 22:31:11 +0300

    windows fix

--- agio/core/utils/app_dirs.py
+++ agio/core/utils/app_dirs.py
@@ -36,7 +36,7 @@ def get_default_env_dir():
     if from_env:
         return Path(from_env)
     if os.name == 'nt':
-        return Path(os.getenv('APPDATA'), 'agio\default-env').as_posix()
+        return Path(os.getenv('APPDATA'), r'agio\default-env').as_posix()
     elif os.name == "posix":
         return Path('~/.config/agio/default-env').expanduser().absolute().as_posix()
     else:
