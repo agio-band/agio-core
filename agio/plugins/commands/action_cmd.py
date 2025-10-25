750a5e561f631c4141c4c5bded5d963806512566
admin <paulwinex@gmail.com>
2025-10-25 15:29:10 +0300

    added get-actions command

--- agio/plugins/commands/action_cmd.py
+++ agio/plugins/commands/action_cmd.py
@@ -5,30 +5,41 @@ import click
 
 from agio.core.utils.args_helper import parse_args_to_dict
 from agio.core.utils import actions, process_utils
+from agio.core.utils import get_actions
+
 
 class ActionCommand(ACommandPlugin):
     name = 'action_cmd'
     command_name = 'action'
-    context_settings = dict(ignore_unknown_options=True)
+    allow_extra_args = True
     arguments = [
         click.argument('action'),
-        click.argument('args', nargs=-1)
+        # click.argument('args', nargs=-1) -> use __extra_args__ instead
     ]
     help = 'Execute any action'
 
-    def execute(self, action, args: tuple|list):
-        print('ACTION:', action)
-        if args:
-            args: dict = parse_args_to_dict(args)
-        print('KWARGS:', args)
+    def execute(self, action, **kwargs):
+        args, kwargs = self.parse_extra_args(kwargs)
         # find action
         action_func = actions.get_action_func(action)
         # execute and get result
-        result = action_func(**args)
-        # write to pipe is defined
-        if process_utils.pipe_is_allowed():
-            with process_utils.data_pipe('w') as pipe:
-                pipe.write(json.dumps(result))
-        # done
+        result = action_func(*args, **kwargs)
         return result
+
+
+class GetActionsCommand(ACommandPlugin):
+    name = 'get_actions_cmd'
+    command_name = 'get-actions'
+    allow_extra_args = True
+    allow_write_output_to_custom_pipe = True    # temporary forced write to custom pipe
+
+    arguments = (
+        click.option('-m', '--menu-name', default=None, help='Manu Name'),
+        click.option('-a', '--app-name', default=None, help='App Name'),
+    )
+
+    def execute(self, menu_name, app_name, **kwargs):
+        action_group = get_actions(menu_name, app_name)
+        all_actions = action_group.serialize()
+        return all_actions
 
