5867066dcc7d456b7d25c5c254286a74ba9007d7
admin <paulwinex@gmail.com>
2025-11-03 19:04:33 +0300

    fix core settings

--- agio/package_settings/local_settings.py
+++ agio/package_settings/local_settings.py
@@ -3,5 +3,5 @@ from agio.tools import app_dirs
 
 
 class CoreSettings(APackageSettings):
-    workspaces_root: str = lambda: app_dirs.default_env_dir().as_posix()  # TODO add supporting annotation str|Path
+    workspaces_root: str = lambda: app_dirs.default_env_install_dir().as_posix()  # TODO add supporting annotation str|Path
 
