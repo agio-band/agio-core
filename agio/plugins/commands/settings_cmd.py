cd56b99898c740a7f3c9c9d2e132d8074d9eea7e
admin <paulwinex@gmail.com>
2026-06-08 21:14:27 +0300

    update SettingsCommand WIP

--- agio/plugins/commands/settings_cmd.py
+++ agio/plugins/commands/settings_cmd.py
@@ -1,3 +1,8 @@
+import json
+import textwrap
+
+import click
+
 from agio.core.plugins.base_command import ACommandPlugin
 
 
@@ -5,29 +10,53 @@ class SettingsCommand(ACommandPlugin):
     name = 'settings_cmd'
     command_name = 'settings'
     arguments = [
-        # click.option('-k', '--key', help='Show value of key'),
+        click.option('-s', '--skip-defaults', help='Skip Default Values', is_flag=True),
+        click.option('-p', '--project_id', required=False, help='Project ID'),
+        click.argument('packages', nargs=-1, required=False),
     ]
     help = 'Settings info'
 
-    def execute(self):
+    def execute(self, skip_defaults, project_id, packages):
         from agio.core.settings import local_settings
 
-        self._print_settings_from_hub(local_settings.load(), 'LOCAL SETTINGS')
-        # self._print_settings_from_hub(workspace_settings, 'WORKSPACE SETTINGS')
+        self._print_settings_from_hub(
+            local_settings.get_settings_dir(project_id),
+            local_settings.load(project_id),
+            'LOCAL SETTINGS',
+            project_id,
+            skip_defaults,
+            packages,
+        )
 
-    def _print_settings_from_hub(self, hub, title: str):
+    def _print_settings_from_hub(self, settings_dir, hub, title: str, project_id=None, skip_defaults=False, packages=None):
         line = lambda: print("=" * 50)
 
         line()
-        print(title)
+        click.secho(title, fg="yellow")
+        print('Location:', settings_dir)
+        if project_id:
+            print('Project ID:', project_id)
         line()
         max_len = 0
         for name, pkg in hub.iter_package_settings():
+            if packages and name not in packages:
+                continue
             for field_name, field in pkg.iter_fields():
                 max_len = max(max_len, len(field_name))
+        printed_count = 0
         for name, pkg in hub.iter_package_settings():
-            print(f"{pkg.name}:")
+            if packages and name not in packages:
+                continue
+            settings = pkg.__dump_settings__(skip_default=skip_defaults)
+            if not settings:
+                continue
+            click.secho(f"{pkg.name}:", fg="green", bold=True)
+            for field_name, field in settings.items():
+                str_value = json.dumps(field['value'], indent=2)
+                print_value = textwrap.indent(str_value, ' '*(max_len+4))[(max_len+4):]
+                print(f"  {field_name:>{max_len}} = {print_value}")
             print('-' * 50)
-            for field_name, field in pkg.iter_fields():
-                print(f"  {field_name:>{max_len}} = {repr(field.get())}")
+            printed_count += 1
+        if not printed_count:
+            click.secho("Nothing to print!", fg="red")
         line()
