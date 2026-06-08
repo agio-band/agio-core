57700355bcec570c9f578c8ea04e6b0e785ea5c1
admin <paulwinex@gmail.com>
2026-06-08 21:14:20 +0300

    update LauncherCommand

--- agio/plugins/commands/launch_cmd.py
+++ agio/plugins/commands/launch_cmd.py
@@ -1,8 +1,10 @@
+import os
+
 import click
 
 from agio.core.events import emit
 from agio.core.plugins.base_command import ACommandPlugin
-from agio.apps import app_hub
+from agio.apps import app_hub, AApplicationLauncher
 from agio.tools import env_names
 
 
@@ -10,57 +12,57 @@ class LauncherCommand(ACommandPlugin):
     name = 'launch_cmd'
     command_name = "launch"
     arguments = [
-        # click.option("-n", "--app-name", help="App Name"),
-        # click.option("-v", "--app-version", help="App Version"),
-        # click.option("-m", "--app-mode", default='default', help="App Mode"),
-        click.argument("app"),
+        click.option("-a", "--app-name", help="App Name"),
+        click.option("-v", "--app-version", help="App Version"),
+        click.option("-m", "--app-mode", default='default', help="App Mode"),
         click.option("-t", "--task-id"),
         click.option("-d", "--detached", is_flag=True, default=False, help="Start Detached"),
         click.option("-c", "--new-console", is_flag=True, default=False, help="Open in new console"),
+        click.option("-e", "--env", multiple=True, help="Extra environment variables"),
     ]
     allow_extra_args = True
 
     def execute(self,
-                app: str,
+                app_name: str,
+                app_version: str,
+                app_mode: str,
                 task_id: str,
+                detached: bool = False,
+                new_console: bool = False,
+                env: list[str] = None,
                 __extra_args__: list = None,
                 **kwargs):
-        app_name, app_version, app_mode = app_hub.parse_key(app)
         if not app_name:
             raise click.BadOptionUsage('app-name', 'app-name not set')
         if not app_version:
             raise click.BadOptionUsage('app_version', 'app-version not set')
         click.secho(f'Start app "{app_name}" v{app_version} [{app_mode}]', fg='green')
-        # return self.start_app(app_name, app_version, app_mode, __extra_args__, **kwargs)
-        new_app = app_hub.get_app(app_name, app_version, mode=app_mode)
+        new_app: AApplicationLauncher = app_hub.get_app(app_name, app_version, mode=app_mode)
         if not task_id:
             raise click.BadOptionUsage('task-id', 'Task ID is required')
         if __extra_args__:
             kwargs['cmd_args'] = __extra_args__
         kwargs['cmd_envs'] = {env_names.TASK_ID: task_id}
-        emit('agio_core.start_app.app_created', payload={'app': new_app})
+        if env is not None:
+            kwargs['cmd_envs'].update(self.parse_envs(env))
         kwargs.update(dict(
-            detached = False,
-            replace = False,
-            new_console = False,
-            non_blocking = False,
+            detached = detached,
+            replace = not new_console,
+            new_console = new_console,
+            non_blocking = new_console,
             exit_on_done = False,
         ))
+        emit('agio_core.start_app.app_created', payload={'app': new_app, 'kwargs': kwargs})
         pid = new_app.start(**kwargs)
+        return pid
 
-    # def start_app(self, app_name, app_version, app_mode, args, **kwargs):
-    #     app = app_hub.get_app(app_name, app_version, mode=app_mode)
-    #     emit('agio_core.start_app.app_created', payload={'app': app})
-    #     # apply default args and envs
-    #     if args:
-    #         app.ctx.add_args(*args)
-    #     # starting...
-    #     pid = app.start(**kwargs)
-    #     # app started
-    #     # !!! this code will not be called if app started with "replace" flag !!!
-    #     # move to app process?
-    #     emit('agio_core.start_app.after_started', payload={'app': app, 'pid': pid})
-    #     return pid
+    def parse_envs(self, envs: list[str]):
+        result = {}
+        for env in envs:
+            key, value = env.split('=')
+            result[key] = value
+        return result
+
 
 
 
